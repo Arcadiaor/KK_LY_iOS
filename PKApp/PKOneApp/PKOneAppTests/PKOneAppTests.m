c64dd50549173a950173f4260f3d73092e70fd7e
Arcadia <yonglei0219@163.com>
2019-06-11 22:46:24 +0800

    no message

---
+++ PKApp/PKOneApp/PKOneAppTests/PKOneAppTests.m
@@ -0,0 +1,37 @@
+//
+//  PKOneAppTests.m
+//  PKOneAppTests
+//
+//  Created by leiyong on 2019/5/14.
+//  Copyright © 2019 Arcadia. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PKOneAppTests : XCTestCase
+
+@end
+
+@implementation PKOneAppTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
